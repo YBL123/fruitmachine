9151fba308384a49f74a4473ae35dd39a90cf1e6
YardenLawson <yardenlawson@gmail.com>
2020-10-01 11:35:37 +0100

    update backend

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,17 @@
 const router = require('express').Router()
 // const auth = require('../controllers/auth')
 const floatValue = require('../controllers/floatValue')
+// const float = require('../models/float')
 // const secureRoute = require('../lib/secureRoute')
 
+router.route('/float')
+  .post(floatValue.createFloat)
+
 router.route('/value')
-  .get(floatValue.getValue)
+  .get(floatValue.getFloatValue)
+
+router.route('/value/:id')
+  .put(floatValue.updateFloat)
 
 
 module.exports = router
--- controllers/floatValue.js
+++ controllers/floatValue.js
@@ -1,7 +1,8 @@
 const axios = require('axios')
 const Float = require('../models/float')
+const { notFound, unauthorized } = require('../lib/errorMessages')
 
-async function getValue(req, res) {
+async function getFloatValue(req, res) {
   console.log('heyyyyy')
   try {
     const value = await Float.find()
@@ -11,6 +12,36 @@ async function getValue(req, res) {
   }
 }
 
+async function updateFloat(req, res, next) {
+  const floatId = req.params.id 
+  try {
+    const float = await Float.findById(floatId)
+    if (!float) throw new Error(notFound)
+    Object.assign(float, req.body)
+    await float.save()
+    res.status(202).json(float)
+  } catch (err) {
+    next(err)
+  }
+}
+
+async function createFloat(req, res, next) {
+  const float = await Float.find()
+  console.log(float)
+  if (float.length > 1) {
+    res.sendStatus(409)
+  } else {
+    try {
+      const createdFloat = await Float.create(req.body)
+      res.status(201).json(createdFloat)
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
 module.exports = {
-  getValue
+  getFloatValue,
+  updateFloat,
+  createFloat
 }
